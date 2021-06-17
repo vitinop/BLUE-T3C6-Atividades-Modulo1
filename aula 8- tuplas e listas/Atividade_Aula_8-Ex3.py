45bb798a225410560cca64c0f49a50aef50e5ccd
vitinop <victor.fernando.moura@hotmail.com>
2021-06-17 14:57:41 -0300

    exercicios resolvidos

---
+++ aula 8- tuplas e listas/Atividade_Aula_8-Ex3.py
@@ -0,0 +1,7 @@
+# 03 - Dada a lista l = [5, 7, 2, 9, 4, 1, 3], escreva um programa que imprima as seguintes informações:
+# a) tamanho da lista.
+# b) maior valor da lista.
+# c) menor valor da lista.
+# d) soma de todos os elementos da lista.
+# e) lista em ordem crescente.
+# f) lista em ordem decrescente.
