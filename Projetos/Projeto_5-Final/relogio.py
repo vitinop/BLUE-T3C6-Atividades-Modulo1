19f6f582e88e1f54d0b136acd1b85d610353b6de
vitinop <victor.fernando.moura@hotmail.com>
2021-07-02 20:46:42 -0300

    Update relogio.py

--- Projetos/Projeto_5-Final/relogio.py
+++ Projetos/Projeto_5-Final/relogio.py
@@ -6,10 +6,13 @@ class Relogio:
     def __str__(self): # Pesquisar significado dessa função especifica.
         return f"{self.horas:02d}:{self.minutos:02d}"
     
-    def avancaTempo(self, minutos,horas):
+    def avancaTempo(self, horas,minutos,dia):
         self.horas+= horas
         self.minutos += minutos
         while(self.minutos >= 60):
             self.minutos -= 60
             self.horas += 1
+        while(self.horas)>24:
+            self.horas-=24
+            self.dia+=1
     
